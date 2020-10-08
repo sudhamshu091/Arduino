3c90e22652bd3be428e7bc540fc950b793ba7006
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-09 00:38:26 +0530

    Add files via upload

---
+++ lcd_display_push_button.ino
@@ -0,0 +1,17 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+int pushButton = 7;
+
+void setup(){
+  lcd.begin(16,2);
+  pinMode(pushButton,INPUT);
+}
+void loop(){
+  if(digitalRead(pushButton)== HIGH)
+  {
+  lcd.setCursor(0,0);
+  lcd.print("Button Pressed");
+  lcd.setCursor(0,1);
+  lcd.print("Sudhamshu B N");
+  }
+}
