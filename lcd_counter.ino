6f1c368566f6326e55b514f25f797f7f8e3c67cf
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-09 00:43:58 +0530

    Add files via upload

---
+++ lcd_counter.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+int pushButton = 7;
+int count = 0;
+
+void setup(){
+  lcd.begin(16,2);
+  pinMode(pushButton,INPUT);
+}
+void loop(){
+  if(digitalRead(pushButton)== HIGH)
+  {
+  lcd.setCursor(0,0);
+  lcd.print("Button Pressed");
+  lcd.setCursor(0,1);
+  lcd.print(count++);
+  }
+}
