e14a1df93e14b9b0c96696af5d7f0e5a2ed6c6e7
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-08 23:25:51 +0530

    Add files via upload

---
+++ tricolour_led.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(10, LOW);
+  delay(500);
+  digitalWrite(11, HIGH);
+  delay(500);
+  digitalWrite(11, LOW);
+  delay(500);
+  digitalWrite(12, HIGH);
+  delay(500);
+  digitalWrite(12, LOW);
+  delay(500);
+}
