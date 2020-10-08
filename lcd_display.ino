9003195aa70b98ecd7c48d5ec767caca47db3827
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-09 00:17:11 +0530

    Add files via upload

---
+++ lcd_display.ino
@@ -0,0 +1,13 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+
+void setup(){
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Sudhamshu B N");
+  lcd.setCursor(0,1);
+  lcd.print("Kasaragod");
+}
+void loop(){
+}
