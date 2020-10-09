6e702b38997fc79ade7226f22d61fe3aa19ca067
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-10-09 22:14:59 +0530

    Add files via upload

---
+++ PWM_LED_brightness.ino
@@ -0,0 +1,17 @@
+int LED_Pin = 9;
+int duty_cycle_value = 1;
+
+void setup()
+{
+  pinMode(LED_Pin, OUTPUT);
+}
+
+void loop()
+{
+  while (duty_cycle_value<255)
+  {
+    analogWrite(LED_Pin,duty_cycle_value);
+    delay(3000);
+    duty_cycle_value++;
+  }
+}
